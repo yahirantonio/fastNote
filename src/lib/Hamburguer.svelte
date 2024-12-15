4ebae1ee8386028adccf8b25f9eca4fb06c1c31c
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-15 17:24:25 -0600

    feat(hamburguer): google icons agregado y componente terminado

---
+++ src/lib/Hamburguer.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<span class="material-symbols-outlined expand"> menu </span>
+
+<style>
+   .expand {
+      font-size: 2.5rem;
+   }
+</style>
--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,9 +1,9 @@
 <script>
-
+   import Hamburguer from "../lib/Hamburguer.svelte";
 </script>
 
+<Hamburguer />
 <h1>Home</h1>
 
 <style lang="postcss">
-
 </style>
