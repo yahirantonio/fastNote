2ee3c6cc2d19852ea2ca7514e852135b33eb3630
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-15 10:42:52 -0600

    feat(tailwind.config): propiedades personalizadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        berkshire_swash: ['Berkshire Swash', 'serif'],
+        montserrat: ['Montserrat', 'serif'],
+        lato: ['Lato', 'serif']
+      },
+      fontSize:{
+        titles: '2.5rem',
+        title_card: '1.5rem',
+        body_card: '.875rem',
+        tag_card: '.625rem',
+        date_card: '.875rem',
+        pills: '1.25rem',
+        links: '1rem'
+      }
+    },
   },
   plugins: [],
 }
