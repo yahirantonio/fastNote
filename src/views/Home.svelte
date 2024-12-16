381130d5d8792554f2a8eee18ee1f1a617431b20
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-16 14:48:03 -0600

    feat(home): header

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -4,10 +4,21 @@
    import Search from "../lib/Search.svelte";
 </script>
 
-<Hamburguer />
-<Search />
-<Date />
-<h1>Home</h1>
+<div class="mx-20">
+   <header class="flex justify-between w-100 pt-3">
+      <div class="flex-none mr-20">
+         <Hamburguer />
+      </div>
+      <div class="flex-1">
+         <Search />
+      </div>
+      <div class="flex-none">
+         <Date />
+      </div>
+   </header>
+   
+   <h1>Home</h1>
+</div>
 
 <style lang="postcss">
 </style>
