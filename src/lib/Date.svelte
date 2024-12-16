5b83f9fc5dfc6aad158baea0015a7d51737a3d48
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-16 14:26:20 -0600

    feat(date): flatpicker y nuevo componente date

---
+++ src/lib/Date.svelte
@@ -0,0 +1,18 @@
+<script>
+   import flatpickr from "flatpickr";
+   import { onMount } from "svelte";
+   import "flatpickr/dist/themes/dark.css";
+
+   let date;
+   onMount(() => {
+      flatpickr(date, {
+         wrap: true
+      });
+   });
+</script>
+
+<div class="flatpickr w-fit flex flex-direction-center" bind:this={date}>
+   <span class="material-symbols-outlined absolute text-white"> calendar_today </span>
+   <input type="text" name="" id="" data-input class="bg-black rounded-md cursor-pointer text-white ps-8"/>
+   <span class="material-symbols-outlined relative text-white right-7"> keyboard_arrow_up </span>
+</div>
--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,10 +1,12 @@
 <script>
+   import Date from "../lib/Date.svelte";
    import Hamburguer from "../lib/Hamburguer.svelte";
    import Search from "../lib/Search.svelte";
 </script>
 
 <Hamburguer />
 <Search />
+<Date />
 <h1>Home</h1>
 
 <style lang="postcss">
