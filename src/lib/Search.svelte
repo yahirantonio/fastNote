d965946b0a8934143d7b7a31b73b9ed6e8f54de9
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-16 14:52:28 -0600

    fix(search): tamaño corregido

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -11,12 +11,12 @@
    onmouseenter={() => (mouseEnter = true)}
    onmouseleave={() => (mouseEnter = false)}
 >
-   <span class="material-symbols-outlined text-3xl absolute"> search </span>
+   <span class="material-symbols-outlined text-4xl absolute"> search </span>
    <input
       type="text"
       name="title"
       id="title"
-      class="bg-white shadow-elevated_shadow text-2xl rounded-lg ps-8 transition-all ease-in-out duration-1000"
+      class="bg-white shadow-elevated_shadow text-2xl rounded-lg ps-9 transition-all ease-in-out duration-1000 p-1"
       style="width: {expand ? '100%' : '20%'}"
       bind:value={title}
    />
