f9fbc2d26e3d696c954e2d56dad64e3ccfd1b4ad
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-16 15:01:17 -0600

    fix(Date): tamaños correctos

--- src/lib/Date.svelte
+++ src/lib/Date.svelte
@@ -6,13 +6,14 @@
    let date;
    onMount(() => {
       flatpickr(date, {
-         wrap: true
+         wrap: true,
+         defaultDate: new Date()
       });
    });
 </script>
 
-<div class="flatpickr w-fit flex flex-direction-center" bind:this={date}>
-   <span class="material-symbols-outlined absolute text-white"> calendar_today </span>
-   <input type="text" name="" id="" data-input class="bg-black rounded-md cursor-pointer text-white ps-8"/>
-   <span class="material-symbols-outlined relative text-white right-7"> keyboard_arrow_up </span>
+<div class="flatpickr w-fit flex flex-direction-center relative h-full" bind:this={date}>
+   <span class="material-symbols-outlined absolute text-white text-4xl left-2 top-1"> calendar_today </span>
+   <input type="text" name="" id="" data-input class="bg-black rounded-md cursor-pointer text-white ps-16 p-1 text-xl h-full w-60"/>
+   <span class="material-symbols-outlined absolute text-white right-2 top-1 text-4xl"> keyboard_arrow_up </span>
 </div>
