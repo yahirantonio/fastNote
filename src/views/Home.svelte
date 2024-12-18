fd365031f41dede05611622fd905b520b2c6daf5
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-18 16:25:35 -0600

    feat(notes, home), agregando Notas a la vista, peakSequence terminado

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -2,6 +2,24 @@
    import Date from "../lib/Date.svelte";
    import Hamburguer from "../lib/Hamburguer.svelte";
    import Search from "../lib/Search.svelte";
+   import Note from "../lib/Note.svelte";
+   import { dataNotes } from "../stores/notes.svelte";
+
+   let notesShow = $dataNotes.slice(0, 5);
+
+   let noteSelected = $state(null);
+
+   $inspect(noteSelected);
+
+   let peakSequence = $derived.by(() => {
+      let length = notesShow.length;
+      let half = Math.round(notesShow.length / 2);
+      let rest = length - half;
+      let sequence = Array.from({ length: length }, (_, i) =>
+         i < half ? i + 1 : rest--,
+      );
+      return sequence;
+   });
 </script>
 
 <header class="flex justify-between w-100 pt-3">
@@ -16,7 +34,20 @@
    </div>
 </header>
 
-<h1 class="font-berkshire_swash text-4xl text-center mt-10">Home</h1>
+<h1 class="font-berkshire_swash text-4xl text-center my-10">Home</h1>
 
-<style lang="postcss">
-</style>
+<div class="flex justify-center">
+   {#each notesShow as notes, i}
+      <div
+         style:margin-left={i == 0 ? "0px" : "-150px"}
+         style:z-index={i == noteSelected ? 99 : peakSequence[i]}
+         role="button"
+         tabindex="0"
+         onmouseenter={() => (noteSelected = i)}
+         onmouseleave={() => (noteSelected = null)}
+         
+      >
+         <Note {...notes} />
+      </div>
+   {/each}
+</div>
