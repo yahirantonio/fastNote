2a1d50f7fc39870d0d1e5fce59097fc1225cab49
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-19 14:08:40 -0600

    style(pills): bakcground color corregido

--- src/lib/animation/Pills.svelte
+++ src/lib/animation/Pills.svelte
@@ -5,14 +5,14 @@
 </script>
 
 <div class="mb-10 flex justify-center gap-6 text-white">
-   <div class="bg-red-600 pills" in:fly={{ x: 150, duration }}>
-      <p>Por hacer</p>
+   <div class="shadow-2 pills" in:fly={{ x: 150, duration }}>
+      <p>Por Hacer</p>
    </div>
-   <div class="bg-green-600 pills" in:fly={{ x: 0, duration }}>
-      <p>Por hacer</p>
+   <div class="shadow-1 pills" in:fly={{ x: 0, duration }}>
+      <p>Hecho</p>
    </div>
-   <div class="bg-blue-600 pills" in:fly={{ x: -150, duration }}>
-      <p>Por hacer</p>
+   <div class="shadow-0 pills" in:fly={{ x: -150, duration }}>
+      <p>En Progreso</p>
    </div>
 </div>
 
@@ -23,5 +23,19 @@
       font-size: 1.25rem;
       font-weight: 600;
       box-shadow: 0px 10px 40px -5px rgba(0, 0, 0, 0.25);
+      min-width: 120px;
+      text-align: center;
+   }
+
+   .shadow-0 {
+      background-color: #28C9CC;
+   }
+
+   .shadow-1 {
+      background-color: #52CF1B;
+   }
+
+   .shadow-2 {
+      background-color: #DE4C4C;
    }
 </style>
