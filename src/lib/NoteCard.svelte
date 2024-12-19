8d2244b5035ffc366495c8990d67cce4634f60b3
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-19 14:03:19 -0600

    fix(noteCard): noteCard not included

---
+++ src/lib/NoteCard.svelte
@@ -0,0 +1,53 @@
+<script>
+   const {
+      notaID,
+      titulo = "sin titulo...",
+      texto = "sin texto...",
+      fecha = new Date(),
+      tag = "no tag",
+      estadoID = 0,
+   } = $props();
+
+   let isSelected = $state(false);
+
+   function limitText(text) {
+      return text.length > 250 ? text.slice(0, 250) + "..." : text;
+   }
+</script>
+
+<div
+   class="bg-white rounded-xl shadow-{estadoID} p-4 w-80 h-80 flex flex-col transition-all duration-1000"
+   id="card"
+   role="button"
+   tabindex="0"
+   onmouseenter={() => (isSelected = true)}
+   onmouseleave={() => (isSelected = false)}
+   style={isSelected && "transform: scale(1.2);"}
+>
+   <div>
+      <h2 class="font-lato text-2xl font-bold mb-3">{titulo}</h2>
+   </div>
+   <div class="flex-1">
+      <p class="font-montserrat text-sm mb-10">{limitText(texto)}</p>
+   </div>
+   <div class="flex justify-between">
+      <p class="font-montserrat text-sm">{fecha}</p>
+      <div>
+         <p class="font-montserrat text-xs">{tag}</p>
+      </div>
+   </div>
+</div>
+
+<style>
+   .shadow-0 {
+      box-shadow: 0px 10px 40px -5px rgba(153, 253, 255, .5);
+   }
+
+   .shadow-1 {
+      box-shadow: 0px 10px 40px -5px rgba(184, 255, 153, .5);
+   }
+
+   .shadow-2 {
+      box-shadow: 0px 10px 40px -5px rgba(255, 153, 153, .5);
+   }
+</style>
