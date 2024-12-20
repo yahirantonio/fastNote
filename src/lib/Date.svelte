136fde1eb5c685b555bf1196337c471a18bd097b
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-20 06:37:04 -0600

    style(date): alto fijado

--- src/lib/Date.svelte
+++ src/lib/Date.svelte
@@ -16,8 +16,8 @@
    });
 </script>
 
-<div class="flatpickr w-fit flex flex-direction-center relative h-full" bind:this={dateDiv}>
-   <span class="material-symbols-outlined absolute text-white text-4xl left-2 top-1"> calendar_today </span>
+<div class="flatpickr w-fit flex flex-direction-center relative h-11" bind:this={dateDiv}>
+   <span class="material-symbols-outlined absolute text-white text-4xl left-2 top-0.5"> calendar_today </span>
    <input type="text" name="" id="" data-input class="bg-black rounded-md cursor-pointer text-white ps-16 p-1 text-xl h-full w-60" bind:value={date}/>
-   <span class="material-symbols-outlined absolute text-white right-2 top-1 text-4xl"> keyboard_arrow_up </span>
+   <span class="material-symbols-outlined absolute text-white right-2 top-0.5 text-4xl"> keyboard_arrow_up </span>
 </div>
